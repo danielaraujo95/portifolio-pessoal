40998016d7a0e92bd830f7748407ab4be1656452
Daniel Araújo <80237474+danielaraujo95@users.noreply.github.com>
2022-04-03 20:47:47 -0300

    update CSS

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 // Como eu fiz o Scroll suave <3
-$(".menu-nav a").click(function(e){
-    e.preventDefault();
-    var id = $(this).attr('href');
-    console.log(id);
-    targetOffset = $(id).offset().top;
-    $("html,bady").animate({
-        scrollTop: targetOffset + 78}, 500);
-
+$('.menu-nav a').click(function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+  console.log(id);
+  targetOffset = $(id).offset().top;
+  $('html,bady').animate(
+    {
+      scrollTop: targetOffset - 86,
+    },
+    500,
+  );
 });
